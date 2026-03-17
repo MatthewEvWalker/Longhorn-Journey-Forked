1eaf8e53f67b137a8788792ef56aa5185b38aa57
Matthew Walker <108114157+MatthewEvWalker@users.noreply.github.com>
2026-03-17 00:56:21 -0500

    Need to merge my newest push (#141)
    * adding tsconfig.json
    
    * adding express server, routes, and placeholder pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,31 @@
-import InterestSelection from './InterestSelection';
+// import CreateAccount from "./CreateAccount";
+
+// export default function Index() {
+//   return <CreateAccount />;
+// }
+
+import { router } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  return <InterestSelection />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Longhorn Journey</Text>
+      <Button title="Enter App" onPress={() => router.push("/home")} />{" "}
+      {/* router.replace("/(tabs)/home"); once they're done with onboarding so they dont go back*/}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 20,
+  },
+});
